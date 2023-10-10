d5bc3ef4e2293aa8b540ec7bd46065670a335cdc
Avalos Pucyura <mmavalos@indracompany.com>
2023-10-09 22:37:44 -0500

    proyecto php-inicio

---
+++ base.php
@@ -0,0 +1,25 @@
+<?php
+        $inicio = false;
+    include './includes/templates/header.php';
+?>
+
+    <main class="contenedor seccion">
+        <h1>Titulo Página</h1>
+    </main>
+
+    <footer class="footer seccion">
+        <div class="contenedor contenedor-footer">
+            <nav class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+            </nav>
+        </div>
+
+        <p class="copyright">Todos los derechos Reservados 2021 &copy;</p>
+    </footer>
+
+    <script src="build/js/bundle.min.js"></script>
+</body>
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes Raices</title>
+    <link rel="stylesheet" href="build/css/app.css">
+</head>
+<body>
+    
+    <header class="header <?php echo $inicio ? 'Inicio' : '' ; ?>">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="">
+                    <img src="build/img/logo.svg" alt="Logotipo de Bienes Raices">
+                </a>
+
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                </div>
+
+                <div class="derecha">
+                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
+                    <nav class="navegacion">
+                        <a href="nosotros.php">Nosotros</a>
+                        <a href="anuncios.php">Anuncios</a>
+                        <a href="blog.php">Blog</a>
+                        <a href="contacto.php">Contacto</a>
+                    </nav>
+                </div>
+   
+                
+            </div> <!--.barra-->
+            <?php   if($inicio){    ?>
+                <h1>Venta de Casas y Departamentos  Exclusivos de Lujo</h1>
+            <?php } ?>
+        </div>
+    </header>
